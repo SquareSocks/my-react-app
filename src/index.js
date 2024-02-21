f05410f8a17ee3357ac0ae30d960e64e028f2839
Me <squaresockstech@gmail.com>
2024-02-21 12:33:25 -0600

    after completing React JSX chapter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { name,age } from "./person.js";
-import message from "./message.js";
-
-
-const myFirstElement = <h1>Hello React!</h1>
-
-// how to make and then print each element of an array
-// don't forget to render the result but changing root.render(myList)
-const myArray = ['apple', 'banana', 'orange'];
-const myList = myArray.map((item) => <p>{item}</p>)
-
-
-
+const myelement = (
+    <table>
+      <tr>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <td>John</td>
+      </tr>
+      <tr>
+        <td>Elsa</td>
+      </tr>
+    </table>
+  );
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(myList);
+root.render(myelement);
--- src/person.js
+++
@@ -1,8 +0,0 @@
-const name = "Jesse"
-const age = 40
-
-export { name, age }
-
-// it could have been done with each line individually exported.
-//export const name = "Jesse"
-//export const age = 40
