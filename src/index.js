bf5a94d3dab52e13195ca955dbf167b92ebd37a5
Me <squaresockstech@gmail.com>
2024-02-21 12:13:31 -0600

    After completing the React ES6 chapter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { name,age } from "./person.js";
+import message from "./message.js";
+
+
 const myFirstElement = <h1>Hello React!</h1>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+// how to make and then print each element of an array
+// don't forget to render the result but changing root.render(myList)
+const myArray = ['apple', 'banana', 'orange'];
+const myList = myArray.map((item) => <p>{item}</p>)
+
+
+
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(myList);
---
+++ src/person.js
@@ -0,0 +1,8 @@
+const name = "Jesse"
+const age = 40
+
+export { name, age }
+
+// it could have been done with each line individually exported.
+//export const name = "Jesse"
+//export const age = 40
