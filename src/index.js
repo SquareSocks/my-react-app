ed9a2aac88bd74d72ff5f88d99528be77554b124
Me <squaresockstech@gmail.com>
2024-02-21 20:29:51 -0600

    completed React Lists chapter

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function MissedGoal() {
-  return <h1>MISSED!</h1>;
+function Car(props) {
+  return <li>I am a {props.brand}</li>;
 }
 
-function MadeGoal() {
-  return <h1>Goal!</h1>;
-}
-
-function Goal(props) {
-  //const isGoal = props.isGoal;
-  return <>{props.isGoal ? <MadeGoal /> : <MissedGoal />}</>;
-}
-
-function Garage(props) {
-  const cars = props.cars;
+function Garage() {
+  const cars = [
+    { id: 1, brand: "Ford" },
+    { id: 2, brand: "BMW" },
+    { id: 3, brand: "Audi" },
+  ];
   return (
     <>
-      <h1>Garage</h1>
-      {cars.length > 0 && (
-        <h2>You have {cars.length} cars in your garage. Which is many cars.</h2>
-      )}
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => (
+          <Car key={car.id} brand={car.brand} />
+        ))}
+      </ul>
     </>
   );
 }
-
-const cars = ["Ford", "BMW", "Audi"];
 
 /* This is a expanded way to get root
 const container = document.getElementById('root');
@@ -34,4 +29,4 @@ const root = ReactDOM.createRoot(container);
 */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Goal isGoal={true} />);
+root.render(<Garage />);
