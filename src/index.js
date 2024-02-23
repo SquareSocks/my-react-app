ff0fc1ecfb4ce41b7231f124954a5d65931c5313
Me <squaresockstech@gmail.com>
2024-02-22 19:19:01 -0600

    completed react hooks useContext

--- src/index.js
+++ src/index.js
@@ -1,19 +1,56 @@
-import { useState, useEffect } from "react";
+import { useState, createContext, useContext } from "react";
 import ReactDOM from "react-dom/client";
 
-function Timer() {
-  const [count, setCount] = useState(0);
+const UserContext = createContext();
 
-  useEffect(() => {
-    let timer = setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
+function Component1() {
+  const [user, setUser] = useState("Jesse Hall");
 
-    return () => clearTimeout(timer);
-  }, []);
+  return (
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2 user={user} />
+    </UserContext.Provider>
+  );
+}
 
-  return <h1>I've rendered {count} times!</h1>;
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Timer />);
+root.render(<Component1 />);
